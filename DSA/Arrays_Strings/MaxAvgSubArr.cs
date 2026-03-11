2762756861d91f80ead1715607a3196db6210449
Judd2000 <njudd.mail@gmail.com>
2026-03-11 11:41:30 -0700

    Max Avg Subarray

---
+++ DSA/Arrays_Strings/MaxAvgSubArr.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays_Strings
+{
+    internal class MaxAvgSubArr
+    {
+        //You are given an integer array nums consisting of n elements, and an integer k.
+
+        //Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.
+        //Any answer with a calculation error less than 10-5 will be accepted.
+        public static double FindMaxAverage(int[] nums, int subArrLen) {
+
+            if (subArrLen > nums.Length) return 0;
+
+            // length of subarray is end - start + 1
+            int left = 0;
+            int right = 0;
+
+            // get subarray bounds to correct length
+            double curSum = nums[right];
+            while (right + 1 < subArrLen) {
+                right++;
+                curSum += nums[right];
+            }
+
+            // while right is not at the end of the array
+            double maxSum = curSum;
+            while (right + 1 < nums.Length) {
+                curSum -= nums[left];
+                left++;
+                right++;
+                curSum += nums[right];
+
+                if (curSum > maxSum) maxSum = curSum;
+            }
+
+            return maxSum / subArrLen;
+        }
+    }
+}
--- DSA/Arrays_Strings/Program.cs
+++ DSA/Arrays_Strings/Program.cs
@@ -1,11 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Arrays_Strings;
-Console.WriteLine("Reverse String - Basic Test Cases");
+Console.WriteLine("*** Reverse String - Basic Test Cases ***");
+
+static void PrintArr<T>(T[] arr) {
+    for (int i = 0; i < arr.Length; i++) {
+        Console.Write($" {arr[i]}");
+    }
+    Console.WriteLine();
+}
 
 char[] inputOne = ['h', 'e', 'l', 'l', 'o'];
 char[] solutionOne = ['o', 'l', 'l', 'e', 'h'];
 
+Console.WriteLine("Input:");
+PrintArr(inputOne);
+
 ReverseString.Reverse(inputOne);
+
+Console.WriteLine($"Received solution:");
+PrintArr(inputOne);
+
+
+Console.WriteLine("Expected output:");
+PrintArr(solutionOne);
 
 bool correctSolution = CharArraysEqual(inputOne, solutionOne);
 
@@ -15,7 +32,17 @@ Console.WriteLine($"Solution one is correct: {correctSolution}");
 char[] inputTwo = ['H', 'a', 'n', 'n', 'a', 'h'];
 char[] solutionTwo = ['h', 'a', 'n', 'n', 'a', 'H'];
 
+Console.WriteLine("Input:");
+PrintArr(inputTwo);
+
 ReverseString.Reverse(inputTwo);
+
+Console.WriteLine($"Received solution:");
+PrintArr(inputTwo);
+
+
+Console.WriteLine("Expected output:");
+PrintArr(solutionTwo);
 
 correctSolution = CharArraysEqual(inputTwo, solutionTwo);
 
@@ -29,3 +56,22 @@ static bool CharArraysEqual(char[] a, char[] b) {
     }
     return true;
 }
+
+Console.WriteLine("******** Max average subarray Basic Test Cases ********");
+
+int[] avgInputOne = [1, 12, -5, -6, 50, 3];
+int inputSubLen = 4;
+
+double expectedAvg = 12.75;
+
+double maxAvg = MaxAvgSubArr.FindMaxAverage(avgInputOne, inputSubLen);
+
+correctSolution = maxAvg == expectedAvg;
+
+Console.WriteLine("Input array:");
+PrintArr(avgInputOne);
+
+Console.WriteLine($"Expected solution: {expectedAvg}");
+Console.WriteLine($"Actual solution: {maxAvg}");
+Console.WriteLine($"Correct solution? {correctSolution}");
+
