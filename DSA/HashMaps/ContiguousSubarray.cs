9aaaa253e9d70bbca83b308d90e6efe555857dc8
Judd2000 <nathaniel_judd@churchofjesuschrist.org>
2026-04-29 10:56:02 -0600

    RansomNote implementation: 26 length array, hash set, hashmap

---
+++ DSA/HashMaps/ContiguousSubarray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HashMaps {
+    internal class ContiguousSubarray
+    {
+
+        public static int FindMaxLength(int[] nums)
+        {
+            Dictionary<int, int> seenSums = new(); // Key: Sum, Value: Index Seen
+
+            int maxLen = 0;
+
+            int curSum = 0;
+
+            seenSums.Add(0, -1); // must add a "baseline"
+
+
+            for (int i = 0; i < nums.Length; i++) {
+                curSum += nums[i] == 1 ? 1 : -1; // subtract 1 if 0, add 1 if 1.
+                if (seenSums.ContainsKey(curSum)) {
+                    maxLen = Math.Max(maxLen, i - seenSums[curSum]);
+                } else {
+                    // Add
+                    seenSums.Add(curSum, i);
+                }
+            }
+            return maxLen;
+        }
+    }
+}
---
+++ DSA/HashMaps/RansomNote.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashMaps;
+internal class RansomNote
+{
+    public static bool CanConstruct_ArrayVer(string ransomNote, string magazine)
+    {
+        int[] letters = new int[26]; // Default is 0
+
+        foreach (char c in magazine)
+        {
+            letters[c - 'a']++;
+        }
+
+        foreach (char c in ransomNote) {
+            if (letters[c - 'a'] > 0)
+            {
+                letters[c - 'a']--;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // SLOW
+    public static bool CanConstruct_HashVer(string ransomNote, string magazine)
+    {
+        HashSet<char> ransomNoteChars = new();
+        foreach (char c in ransomNote) {
+            ransomNoteChars.Add(c);
+        }
+
+        foreach (char c in ransomNoteChars)
+        {
+            if (ransomNote.Count(cha => cha == c) > magazine.Count((cha => cha == c))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // FAST
+    public static bool CanConstruct_HashVerFASTER(string ransomNote, string magazine) {
+
+        Dictionary<char, int> frequency = new();
+
+        foreach (char c in magazine) {
+            if (frequency.TryGetValue(c, out int count))
+            {
+                frequency[c] = count + 1; // Increment existing
+            }
+            else
+            {
+                frequency[c] = 1; // Add new
+            }
+        }
+
+        foreach (char c in ransomNote) {
+            if (frequency.TryGetValue(c, out int count) && count > 0)
+            {
+                frequency[c]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
